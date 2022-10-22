bee7a1d60e39662b750f6f9c724b81ebed61d456
Jean Carlo Montoya Castro <76453032+JeancoMontoyaC@users.noreply.github.com>
2022-10-21 23:33:52 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -4,30 +4,21 @@ from datetime import datetime
 
 def clean_data():
     df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
-    
-    # Eliminación de datos faltantes y duplicados
+ 
     df.dropna(axis = 0, inplace = True)
-    df.drop_duplicates(inplace = True)
 
-    # Poner en minúscula las columnas de tipo texto y eliminar carácteres especiales de idea_negocio y barrio
     for columna in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
         df[columna] = df[columna].str.lower()
         df[columna] = df[columna].apply(lambda x: x.replace('_', ' '))
         df[columna] = df[columna].apply(lambda x: x.replace('-', ' '))
-
-    # Depurar la columna monto_del_credito quitando los carácteres extraños (No numéricos)
-    df['monto_del_credito'] = df['monto_del_credito'].str.replace("\$[\s*]", "")
-    df['monto_del_credito'] = df['monto_del_credito'].str.replace(",", "")
-    df['monto_del_credito'] = df['monto_del_credito'].str.replace("\.00", "")
-    df['monto_del_credito'] = df['monto_del_credito'].astype(int)
+        df[columna]=df[columna].apply(lambda x: x.replace('.', ' '))
     
-    # Corregir tipo de dato de la columna comuna_ciudadano
-    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(float)
+    df['monto_del_credito'] = df['monto_del_credito'].replace(regex="\$[\s*]", value="")
+    df['monto_del_credito'] = df['monto_del_credito'].apply(lambda x: x.replace(",", ""))
+    df['monto_del_credito'] = df['monto_del_credito'].apply(lambda x: x.replace(".00", ""))
 
-    # Corregir tipo de dato de la columna comuna_ciudadano -> Pasar a formato de fecha
-    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
 
-    # Luego de depurar, se pueden haber generado duplicados, por lo que se eliminan de nuevo
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.search("^\d+/", x).group())-1 == 4) else datetime.strptime(x, "%d/%m/%Y"))
     df.drop_duplicates(inplace = True)
 
     return df
